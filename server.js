08f4bb1bb1bad03f806c7e911c134ff7e1e0cae7
miflck <contact@michaelflueckiger.ch>
2025-07-30 20:34:03 +0200

    implement display and rooms

--- server.js
+++ server.js
@@ -1,27 +1,49 @@
-const express = require('express');
-const http = require('http');
-const socketIo = require('socket.io');
-const path = require('path');
+const express = require("express");
+const http = require("http");
+const socketIo = require("socket.io");
+const path = require("path");
 
 const app = express();
 const server = http.createServer(app);
 const io = socketIo(server);
 
 // Serve static files from public directory
-app.use(express.static('public'));
+app.use(express.static("public"));
+
+// Routes
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "index.html"));
+});
+
+app.get("/display", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "display.html"));
+});
 
 // Socket.IO connection handling
-io.on('connection', (socket) => {
-  console.log('User connected:', socket.id);
+io.on("connection", (socket) => {
+  console.log("User connected:", socket.id);
 
   // Handle drawing data
-  socket.on('drawing', (data) => {
-    // Broadcast drawing data to all other clients
-    socket.broadcast.emit('drawing', data);
+  socket.on("drawing", (data) => {
+    // Broadcast to display clients only
+    socket.broadcast.to("display").emit("drawing", data);
+    // Also broadcast to other display clients in case multiple displays
+    io.to("display").emit("drawing", data);
   });
 
-  socket.on('disconnect', () => {
-    console.log('User disconnected:', socket.id);
+  // Handle clear canvas
+  socket.on("clear", () => {
+    io.to("display").emit("clear");
+  });
+
+  // Handle joining display room
+  socket.on("join-display", () => {
+    socket.join("display");
+    console.log("Client joined display room:", socket.id);
+  });
+
+  socket.on("disconnect", () => {
+    console.log("User disconnected:", socket.id);
   });
 });
 
